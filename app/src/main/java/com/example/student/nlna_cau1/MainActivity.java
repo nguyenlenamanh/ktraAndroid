2a05811fa6b082851d88085c7b236c0e7f5901a7
H71M10 <nguyenlenamanh.2016@outlook.com>
2019-08-20 13:31:44 +0700

    first commit

---
+++ app/src/main/java/com/example/student/nlna_cau1/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.student.nlna_cau1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final EditText editText = findViewById(R.id.edit_text_id);
+        final TextView textView = findViewById(R.id.text_view_id);
+        final Button button = findViewById(R.id.button_id);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                textView.setText("Xin chao " + editText.getText());
+            }
+        });
+    }
+}
